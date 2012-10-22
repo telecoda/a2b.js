b49be83e02f27bb13fdc3793fcaa459fa4752add
telecoda <rob@thebaines.co.uk>
2012-10-22 20:37:19 +0100

    remove A2B.Graphics. refs

--- js/game.js
+++ js/game.js
@@ -435,28 +435,28 @@ A2B.Game.prototype.mainMenuModeInitScene = function(currentScene) {
 
 		var wood_material = this.materials["wood"];
 
-		var fontProps = A2B.Graphics.initFontProps();
+		var fontProps = A2B.initFontProps();
 		// create A2B title mesh
-		var a2bMesh = A2B.Graphics.createTextMesh("A2B",wood_material,fontProps);
+		var a2bMesh = A2B.createTextMesh("A2B",wood_material,fontProps);
 		// scale
 		a2bMesh.scale = new THREE.Vector3(0.2,0.2,0.2);
 		// position
 		a2bMesh.position = new THREE.Vector3(-15,5,10);
-		a2bMesh.rotation = new THREE.Vector3(A2B.Graphics.degreesToRadians(-30),0,0);
+		a2bMesh.rotation = new THREE.Vector3(A2B.degreesToRadians(-30),0,0);
 		a2bMesh.castShadow = true;
 		a2bMesh.receiveShadow = true;
 		currentScene.add( a2bMesh );
 
 		// create by Telecoda mesh
-		var fontProps = A2B.Graphics.initFontProps();
+		var fontProps = A2B.initFontProps();
 		fontProps.bend= false;
 		fontProps.size=25;
-		var telecodaMesh = A2B.Graphics.createTextMesh("by Telecoda",wood_material,fontProps);
+		var telecodaMesh = A2B.createTextMesh("by Telecoda",wood_material,fontProps);
 		// scale
 		telecodaMesh.scale = new THREE.Vector3(0.2,0.2,0.2);
 		// position
 		telecodaMesh.position = new THREE.Vector3(-15,5,30);
-		telecodaMesh.rotation = new THREE.Vector3(A2B.Graphics.degreesToRadians(-60),0,0);
+		telecodaMesh.rotation = new THREE.Vector3(A2B.degreesToRadians(-60),0,0);
 		telecodaMesh.castShadow = true;
 		telecodaMesh.receiveShadow = true;
 		currentScene.add( telecodaMesh );
@@ -464,15 +464,15 @@ A2B.Game.prototype.mainMenuModeInitScene = function(currentScene) {
 		// create by start mesh
 		var start_material = this.materials["brick"];
 
-		var fontProps = A2B.Graphics.initFontProps();
+		var fontProps = A2B.initFontProps();
 		fontProps.bend= false;
 		fontProps.size=25;
-		var startMesh = A2B.Graphics.createTextMesh("Start Game!",start_material,fontProps);
+		var startMesh = A2B.createTextMesh("Start Game!",start_material,fontProps);
 		// scale
 		startMesh.scale = new THREE.Vector3(0.2,0.2,0.2);
 		// position
 		startMesh.position = new THREE.Vector3(-15,5,50);
-		startMesh.rotation = new THREE.Vector3(A2B.Graphics.degreesToRadians(-90),0,0);
+		startMesh.rotation = new THREE.Vector3(A2B.degreesToRadians(-90),0,0);
 		startMesh.material.color.setHex(0x0000ff);
 
 		startMesh.castShadow = true;
@@ -493,7 +493,7 @@ A2B.Game.prototype.mainMenuModeInitScene = function(currentScene) {
 	
 
 	// add directional light to scene
-	var dirLight = A2B.Graphics.getDirectionalLight();
+	var dirLight = A2B.getDirectionalLight();
 	dirLight.position.set( 20, 40, 25 );
 	dirLight.target.position.copy( currentScene.position );
 
